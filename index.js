17d2f9f10119c70189f50667977831ea2c208a9c
Nathan Delhaye <the@red.cat>
2015-05-12 22:09:56 +0200

    Allow to have configuration option for tftpd, httpd and dhcpd and add default config

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ var mem_store = require('./lib/memory-store');
 var store = new mem_store('192.168.119.30', '192.168.119.60');
 
 var server = new forge({
+  dhcp : {
     subnet: '192.168.119.0/24',
     range_start: '192.168.119.30',
     range_end: '192.168.119.60',
     routers: [ '192.168.119.1' ],
     nameservers: [ '8.8.8.8', '8.8.4.4' ],
-    save_lease: function(lease, cb){
-      store.save_lease(lease, cb); },
-    get_lease: function(mac_addr, cb){
-      store.get_lease(mac_addr, cb); },
-    get_lease_by_ip: function(ip, cb){
-      store.get_lease_by_ip(ip, cb); },
-    get_next_ip: function(cb){
-      store.get_next_ip(cb); },
-    remove_lease: function(mac_addr, cb){
-      store.remove_lease(mac_addr, cb); },
     host: '192.168.119.1'
+  },
+  
 });
 
